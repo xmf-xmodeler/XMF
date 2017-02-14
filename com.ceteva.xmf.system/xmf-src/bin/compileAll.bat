16a1aa0f4a2503432c2570e12322277e6a33a329
Björn Benner <bjoern.benner@uni-due.de>
2017-02-14 16:46:27 +0100

    command line file for compiling all files in a batch process

---
+++ com.ceteva.xmf.system/xmf-src/bin/compileAll.bat
@@ -0,0 +1,55 @@
+@echo off
+
+set XMFHOME=%1%
+set LIB=%XMFHOME%;%XMFHOME%/../../com.ceteva.xmf.machine/bin
+set EVALUATOR=%XMFHOME%/../xmf-img/compiler.img
+set HEAPSIZE=5000
+set STACKSIZE=50
+set FREEHEAP=20
+set FILENAME=%2%
+set MAXJAVAHEAP=-Xmx200m
+set MAXJAVASTACK=-Xss5m
+set VERSION=2.2
+set JAVA=java
+set LOCALE=-Duser.country=UK
+set BOOTFILE="Boot/CompileAll.o"
+
+%JAVA% %LOCALE% %MAXJAVAHEAP% -cp %LIB% xos.OperatingSystem -image %EVALUATOR% -heapSize %HEAPSIZE% -heapSize %HEAPSIZE% -stackSize %STACKSIZE% -arg filename:%BOOTFILE% -arg user:"%USERNAME%" -arg home:"%XMFHOME%" -arg license:license.lic -arg version:"%VERSION%" %2 %3 %4 %5 %6 %7 %8 %9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
