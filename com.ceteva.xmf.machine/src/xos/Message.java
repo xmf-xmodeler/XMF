c8e56e9a00c312f2a63c03129328522c1c554a36
Tony Clark <t.clark@shu.ac.uk>
2017-10-21 13:07:02 +0100

    Added renderOn and renderOff for the browser.

--- com.ceteva.xmf.machine/src/xos/Message.java
+++ com.ceteva.xmf.machine/src/xos/Message.java
@@ -2,72 +2,68 @@ package xos;
 
 public class Message {
 
-    // A message consists of a name, a unique id and a sequence of
-    // message arguments. Messages are passed by clients via XOS.
+  // A message consists of a name, a unique id and a sequence of
+  // message arguments. Messages are passed by clients via XOS.
 
-    public static final int MAXARGS = 30;
+  public static final int MAXARGS = 30;
 
-    private StringBuffer    name    = new StringBuffer();
+  private StringBuffer    name    = new StringBuffer();
 
-    public int              arity;
+  public int              arity;
 
-    public Value[]          args    = new Value[MAXARGS];
+  public Value[]          args    = new Value[MAXARGS];
 
-    public Message() {
-        name.setLength(0);
-        arity = 0;
+  public Message() {
+    name.setLength(0);
+    arity = 0;
+  }
+
+  public Message(String name, int arity) {
+    this.setName(name);
+    this.arity = arity;
+  }
+
+  public void appendNameChar(char c) {
+    name.append(c);
+  }
+
+  public boolean hasName(String name) {
+    if (name.length() != this.name.length()) return false;
+    for (int i = 0; i < name.length(); i++)
+      if (name.charAt(i) != this.name.charAt(i)) return false;
+    return true;
+  }
+
+  public String name() {
+    return name.toString();
+  }
+
+  public void setArity(int arity) {
+    this.arity = arity;
+  }
+
+  public void setName(String name) {
+    this.name.setLength(0);
+    for (int i = 0; i < name.length(); i++)
+      this.name.append(name.charAt(i));
+  }
+
+  public String toString() {
+    String s = name + "(";
+    for (int i = 0; i < arity; i++) {
+      s = s + args[i];
+      if (i < arity - 1) s = s + ",";
     }
+    return s + ")";
+  }
 
-    public Message(String name, int arity) {
-        this.setName(name);
-        this.arity = arity;
+  public String output() { // debug output that shows parameter values (Jens)
+    String s = name + "(";
+    for (int i = 0; i < arity; i++) {
+      s = s + args[i].strValue();
+      if (i < arity - 1) s = s + ",";
     }
-    
-    public void appendNameChar(char c) {
-        name.append(c);
-    }
-
-    public boolean hasName(String name) {
-        if (name.length() != this.name.length())
-            return false;
-        for (int i = 0; i < name.length(); i++)
-            if (name.charAt(i) != this.name.charAt(i))
-                return false;
-        return true;
-    }
-
-    public String name() {
-        return name.toString();
-    }
-    
-    public void setArity(int arity) {
-        this.arity = arity;
-    }
-
-    public void setName(String name) {
-        this.name.setLength(0);
-        for (int i = 0; i < name.length(); i++)
-            this.name.append(name.charAt(i));
-    }
-
-    public String toString() {
-        String s = name + "(";
-        for (int i = 0; i < arity; i++) {
-            s = s + args[i];
-            if (i < arity - 1)
-                s = s + ",";
-        }
-        return s + ")";
-    }
-
-    public String output() { // debug output that shows parameter values (Jens) 
-        String s = name + "(";
-        for (int i = 0; i < arity; i++) {
-            s = s + args[i].strValue();
-            if (i < arity - 1)
-                s = s + ",";
-        }
-        s = s + ")";
-        return s;
-    }
+    s = s + ")";
+    return s;
+  }
 }
